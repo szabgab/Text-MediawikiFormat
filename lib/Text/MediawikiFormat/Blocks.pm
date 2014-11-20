2b32c05726b38272e42326464b2033e0b22f3ffe
Gabor Szabo <gabor@szabgab.com>
2014-11-20 08:51:19 +0200

    Move package Text::MediawikiFormat::Block to its own file.

--- lib/Text/MediawikiFormat/Blocks.pm
+++ lib/Text/MediawikiFormat/Blocks.pm
@@ -3,6 +3,8 @@ package Text::MediawikiFormat::Blocks;
 use strict;
 use warnings::register;
 
+use Text::MediaWikiFormat::Block;
+
 our $VERSION = '1.02';
 
 sub import
@@ -19,121 +21,6 @@ sub import
 
 		return $class->new( type => $type, @_ );
 	};
-}
-
-package Text::MediawikiFormat::Block;
-
-use Scalar::Util qw( blessed reftype );
-
-sub new
-{
-	my ($class, %args) = @_;
-
-	$args{text} = $class->arg_to_ref (delete $args{text} || '');
-	$args{args} = [$class->arg_to_ref (delete $args{args} || [])];
-
-	bless \%args, $class;
-}
-
-sub arg_to_ref
-{
-	my ($class, $value) = @_;
-	return   $value if ( reftype( $value ) || '' ) eq 'ARRAY';
-	return [ $value ];
-}
-
-sub shift_args
-{
-	my $self = shift;
-	my $args = shift @{ $self->{args} };
-	return wantarray ? @$args : $args;
-}
-
-sub all_args
-{ 
-	my $args = $_[0]{args};
-	return wantarray ? @$args : $args;
-}
-
-sub text
-{
-	my $text = $_[0]{text};
-	return wantarray ? @$text : $text;
-}
-
-sub add_text
-{
-	my $self = shift;
-	push @{ $self->{text} }, @_;
-}
-
-sub formatted_text
-{
-	my $self = shift;
-	return map
-	{
-		blessed( $_ ) ? $_ : $self->formatter( $_ )
-	} $self->text();
-}
-
-sub formatter
-{
-	my ($self, $line) = @_;
-	Text::MediawikiFormat::format_line ($line, $self->tags(),
-					    $self->opts());
-}
-
-sub add_args
-{
-	my $self = shift;
-	push @{ $self->{args} }, @_;
-}
-
-{
-	no strict 'refs';
-	for my $attribute (qw( level opts tags type ))
-	{
-		*{ $attribute } = sub { $_[0]{$attribute} };
-	}
-}
-
-sub merge
-{
-	my ($self, $next_block) = @_;
-
-	return $next_block unless $self->type()  eq $next_block->type();
-	return $next_block unless $self->level() == $next_block->level();
-
-	$self->add_text( $next_block->text() );
-	$self->add_args( $next_block->all_args() );
-	return;
-}
-
-sub nests
-{
-	my ($self, $maynest) = @_;
-	my $tags = $self->{tags};
-
-	return exists $tags->{nests}{$self->type()}
-	       && exists $tags->{nests}{$maynest->type()}
-	       && $self->level() < $maynest->level()
-	       # <nowiki> tags nest anywhere, regardless of level and parent
-	       || exists $tags->{nests_anywhere}{$maynest->type()};
-}
-
-sub nest
-{
-	my ($self, $next_block) = @_;
-
-	return unless $next_block = $self->merge ($next_block);
-	return $next_block unless $self->nests ($next_block);
-
-	# if there's a nested block at the end, maybe it can nest too
-	my $last_item = ( $self->text() )[-1];
-	return $last_item->nest( $next_block ) if blessed( $last_item );
-
-	$self->add_text( $next_block );
-	return;
 }
 
 1;
